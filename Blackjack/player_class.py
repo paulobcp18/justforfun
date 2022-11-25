987c073c30c110fe873ae06328f281a3b3e2933e
Paulo <paulobcpimentel18@outlook.com>
2022-11-25 12:11:29 -0300

    Game not working, how can I make it work? How can I make each class interact and work within the game?

--- Blackjack/player_class.py
+++ Blackjack/player_class.py
@@ -40,7 +40,7 @@ class Player:
         print('Player\'s score is: ', self.score)
         return self.score
 
-    def balance(self):
+    def player_bet(self):
         init_bal = self.initial_amount
         print("you have {}, how much do you wanna bet?".format(init_bal))
         self.bet = int(input())
@@ -99,11 +99,81 @@ class Dealer:
 
     def balance(self):
         init_bal = self.initial_amount
+
         
 
+class Blackjack:
+    def __init__(self) -> None:
+        pass
+
+    def game_start():
+        print('Place your bets')        
+        Player.player_bet()
+        print('Cards will be drawn!')
+        Dealer.print_hand()
+        Player.print_hand()
+        Player.players_score()
+
+    def hit_me():
+        print('Do you want another card?')
+        hit_me = input()
+
+        if hit_me == 'yes':
+            Player.hand_update()
+            Player.print_hand()
+            
+            total = Player.players_score()
+            if total > 21:
+                print('bust')
+                Dealer.bet = Player.player_bet[0]
+                Player.player_bet[0] = 0
+                
+            elif total == 21:
+                print('You win!')
+                Player.player_bet[0] = Player.player_bet[0] * 2.5
+
+        else:
+            print('stay')
+            dealer = Dealer.dealers_score()
+            total = Player.players_score()
+            if dealer >= 17:
+                if total > dealer:
+                    print('you win!')
+                    Player.player_bet[0] = Player.player_bet[0] * 2
+                else:
+                    print('you lose!')
+                    Dealer.bet = Player.player_bet[0]
+                    Player.player_bet[0] = 0
+                
+            else:
+                Dealer.hand_update()
+                total_now = Dealer.dealers_score()
+                if total_now > 21:
+                    print('dealer bust!')
+                    Player.player_bet[0] = Player.player_bet[0] * 2
+                else:
+                    total = Player.players_score()
+                    if total_now > total:
+                        ('dealer wins!')
+                        Dealer.bet = Player.player_bet[0]
+                        Player.player_bet[0] = 0
+                    else:
+                        ('you win!')
+                        Player.player_bet[0] = Player.player_bet[0] * 2
+                        
+                
+
+
+
+
 Paulo = Player(1, 1000)
-Paulo.print_hand()
-Paulo.players_score()
+# Paulo.print_hand()
+# Paulo.players_score()
 
 dealer = Dealer()
-dealer.print_hand()
+# dealer.print_hand()
+
+Blackjack.game_start()
+Blackjack.hit_me()
+
+
